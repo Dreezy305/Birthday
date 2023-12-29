9de40544ebc2ba8b8bce8a56b57231930e011c8c
dreezy305 <bankoleidris@gmail.com>
2023-12-29 19:10:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "animate.css";
+import "./App.css";
+import logo from "./asset/mariam.jpeg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ borderRadius: "20px" }}
+        />
+        <p
+          className="birthday animate__animated animate__fadeInLeftBig animate__slow"
+          style={{}}
+        >
+          Happy Birthday to you Babe!
+        </p>
+        <span className="animate__animated animate__fadeInRight animate__slow prayer">
+          Long life and prosperity.
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
