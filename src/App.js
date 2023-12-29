ca0937395bb9e80c67257a8d517b9b19899c0146
dreezy305 <bankoleidris@gmail.com>
2023-12-30 00:23:54 +0100

    birthday fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "animate.css";
-import { useRef } from "react";
-import { Slide } from "react-slideshow-image";
+import { useEffect, useRef, useState } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
+import { Fade, Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import "./App.css";
 import logo from "./asset/mariam.jpeg";
 
 function App() {
+  const [isExploding, setIsExploding] = useState(false);
   const slideRef = useRef();
 
   const properties = {
@@ -20,48 +22,64 @@ function App() {
 
   const slideImages = [logo, logo, logo];
 
+  useEffect(() => {
+    setIsExploding(true);
+  }, []);
+
   return (
-    <div className="App">
-      <Slide ref={slideRef} {...properties}>
-        {slideImages.map((each, index) => (
-          <div
-            key={index}
-            className="each-slide"
-            style={{ paddingTop: "50px" }}
-          >
-            <img
-              className="lazy"
-              src={each}
-              alt="sample"
-              style={{
-                height: "400px",
-                borderRadius: "20px",
-                cursor: "pointer",
-              }}
+    <>
+      <div className="App">
+        <Fade {...properties}>
+          {slideImages.map((each, index) => (
+            <div
+              key={index}
+              className="each-slide"
+              style={{ paddingTop: "50px" }}
+            >
+              <img
+                className="lazy"
+                src={each}
+                alt="sample"
+                style={{
+                  height: "400px",
+                  borderRadius: "20px",
+                  cursor: "pointer",
+                }}
+              />
+            </div>
+          ))}
+        </Fade>
+        <header className="mx">
+          {isExploding && (
+            <ConfettiExplosion
+              width={1600}
+              particleCount={250}
+              duration={6000}
+              force={0.8}
+              height={"120vh"}
+              onComplete={() => setIsExploding(true)}
             />
-          </div>
-        ))}
-      </Slide>
-      <header className="App-headerhh">
-        <div
-          className=""
-          style={{ width: "50%", margin: "auto", paddingBottom: "50px" }}
-        >
-          <p
-            className="birthday animate__animated animate__fadeInLeftBig animate__slow"
-            style={{}}
+          )}
+          <div
+            className=""
+            style={{ width: "50%", margin: "auto", paddingBottom: "50px" }}
           >
-            🎉🎈🎉 Happy Birthday, Babe! 🎉🎈🎉
-          </p>
-          <span className="animate__animated animate__fadeInRight animate__slow prayer">
-            Happy Birthday, my love! 🎉 Your presence lights up my world. Here's
-            to more joy, laughter, and love together. Cheers to your special day
-            and a lifetime of happiness. I love you endlessly
-            ✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂
-          </span>
-        </div>
-      </header>
-    </div>
+            <p
+              className="birthday animate__animated animate__fadeInLeftBig animate__slow"
+              style={{}}
+            >
+              🎉🎈🎉 Happy Birthday, Babe! 🎉🎈🎉
+            </p>
+            <span className="animate__animated animate__fadeInRight animate__slow prayer">
+              Happy Birthday, my love! 🎉 Your presence lights up my world.
+              Here's to more joy, laughter, and love together. Cheers to your
+              special day and a lifetime of happiness. I love you endlessly
+              ✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂✨🎈🌟🎂
+            </span>
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
