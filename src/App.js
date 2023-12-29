c0d22f7835affd24a40ba0982ddb4a43809dfae3
dreezy305 <bankoleidris@gmail.com>
2023-12-29 23:48:50 +0100

    image sliders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import "animate.css";
+import { useRef } from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import "./App.css";
 import logo from "./asset/mariam.jpeg";
 
 function App() {
+  const slideRef = useRef();
+
+  const properties = {
+    duration: 5000,
+    autoplay: true,
+    transitionDuration: 100,
+    arrows: false,
+    infinite: true,
+    easing: "ease",
+    // indicators: (i) => <div className="indicator">{i + 1}</div>,
+  };
+
+  const slideImages = [logo, logo, logo];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          style={{ borderRadius: "20px" }}
-        />
-        <div className="" style={{ width: "50%" }}>
+      <Slide ref={slideRef} {...properties}>
+        {slideImages.map((each, index) => (
+          <div
+            key={index}
+            className="each-slide"
+            style={{ paddingTop: "50px" }}
+          >
+            <img
+              className="lazy"
+              src={each}
+              alt="sample"
+              style={{
+                height: "400px",
+                borderRadius: "20px",
+                cursor: "pointer",
+              }}
+            />
+          </div>
+        ))}
+      </Slide>
+      <header className="App-headerhh">
+        <div
+          className=""
+          style={{ width: "50%", margin: "auto", paddingBottom: "50px" }}
+        >
           <p
             className="birthday animate__animated animate__fadeInLeftBig animate__slow"
             style={{}}
